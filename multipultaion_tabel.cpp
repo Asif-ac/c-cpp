8eb6d74599156be324c0dcb5170af8031783e27f
S M Asifur Rahman <82820113+Asif-ac@users.noreply.github.com>
2021-07-10 08:38:12 +0600

    creat namota

---
+++ multipultaion_tabel.cpp
@@ -0,0 +1,25 @@
+//S M Asifur Rahma
+// khwaja younus Ali University
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout<<"Enter any ineteger: ";
+    cin>>num;
+
+    for(int i=1; i <=10; i++){
+
+        cout<<num << "X" <<i << "=" <<(num*i) <<endl;
+    }
+
+
+
+
+    getch();
+}
